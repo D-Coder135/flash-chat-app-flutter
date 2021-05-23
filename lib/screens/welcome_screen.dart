bb633d0f443cb3d57fce5ede1f744d1589777a02
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-23 20:49:10 +0530

    add a static field inside the class WelcomeScreen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  static String id = 'Welcome_screen';
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
