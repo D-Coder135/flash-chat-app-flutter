90dbfa72f330ee469fc42327a669d21636947d12
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-24 19:24:13 +0530

    add a if block along with a condition inside the method body of the method addStatusListener() method

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -32,7 +32,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
 
     controller.addStatusListener((status) {
       // If we want to loop over our animation i.e. the animation should grow large and then small and then again large and so on =>
-
+      if (status == AnimationStatus.completed) {}
       print(status);
     }); // This method tells you the status for both the forward and reverse animation. It updates the parameter status with a value when the animation is successfully completed.
 
