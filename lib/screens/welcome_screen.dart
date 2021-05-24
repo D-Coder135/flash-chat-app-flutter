a7bbf9bc39ee83c1b198df6db3b371552fc151b0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-24 19:30:32 +0530

    override the dispose() method

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -51,6 +51,12 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
