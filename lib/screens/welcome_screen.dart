e6a7f37ba3c2d5d38d5b6ba145fe8a16ffc643f4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-24 19:40:38 +0530

    comment out some part of the code

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -30,17 +30,17 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     //     from:
     //         1.0); // We you want you animation to grow from large to small i.e. in opposite or reverse order.
 
-    controller.addStatusListener((status) {
-      // If we want to loop over our animation i.e. the animation should grow large and then small and then again large and so on =>
-      if (status == AnimationStatus.completed) {
-        // When the forward() animation is completed, then the value of status parameter is equal to AnimationStatus.completed.
-        controller.reverse(from: 1.0);
-      } else if (status == AnimationStatus.dismissed) {
-        // When the reverse() animation is completed, then the value of status parameter is equal to AnimationStatus.dismissed.
-        controller.forward();
-      }
-      // print(status);
-    }); // This method tells you the status for both the forward and reverse animation. It updates the parameter status with a value when the animation is successfully completed.
+    // controller.addStatusListener((status) {
+    //   // If we want to loop over our animation i.e. the animation should grow large and then small and then again large and so on =>
+    //   if (status == AnimationStatus.completed) {
+    //     // When the forward() animation is completed, then the value of status parameter is equal to AnimationStatus.completed.
+    //     controller.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     // When the reverse() animation is completed, then the value of status parameter is equal to AnimationStatus.dismissed.
+    //     controller.forward();
+    //   }
+    //   // print(status);
+    // }); // This method tells you the status for both the forward and reverse animation. It updates the parameter status with a value when the animation is successfully completed.
 
     controller.addListener(() {
       setState(() {
