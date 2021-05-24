27bb586d7a5eca7e87194ddf03e9764627b6ae85
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-24 18:55:25 +0530

    pass the controller.value property to the height property of the Container() widget containing the image logo

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -45,7 +45,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: controller.value,
                   ),
                 ),
                 Text(
