a55e1c287a69077c922e59dbcc318e583ccd19b3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-24 19:44:25 +0530

    pass the animation.value property to the backgroundColor property of the Scaffold() widget

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -63,7 +63,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
