b7a9d0c3a4ea77894f5c4564ccaee03165b331f0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-24 19:43:49 +0530

    set the value for the height property back to the original value for the Container() widget

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -76,7 +76,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: animation.value * 100.0,
+                    height: 60.0,
                   ),
                 ),
                 Text(
