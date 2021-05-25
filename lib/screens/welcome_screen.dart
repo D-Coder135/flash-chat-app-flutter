4aa0c183fb00c543d35e757dd66e5153a98ea26f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-25 18:30:15 +0530

    add the speed property for the TypewriterAnimatedText() widget

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -89,6 +89,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                         fontSize: 45.0,
                         fontWeight: FontWeight.w900,
                       ),
+                      speed: Duration(milliseconds: 200),
                     ),
                   ],
                 ),
