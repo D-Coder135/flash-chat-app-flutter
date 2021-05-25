0038db6fcca2c126e83dc87a83cca6063334c5e7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 00:27:12 +0530

    call the Firebase.initializeApp() along with the await keyword inside the main() method body

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
@@ -6,6 +7,7 @@ import 'package:flash_chat/screens/chat_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(FlashChat());
 }
 
