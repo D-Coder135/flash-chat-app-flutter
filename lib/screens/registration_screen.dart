dfd61a311d0ccb2368d2452a7db1c124e0ad8e2f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-25 23:24:22 +0530

    import the firebase_auth.dart file inside the file registration_screen.dart

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'Registration_Screen';
