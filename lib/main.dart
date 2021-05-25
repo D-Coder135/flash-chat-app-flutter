e5f40e4836b2b9ee300006fb85efc64df8f1adcb
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 00:24:46 +0530

    add curly braces to the main() method inside the file main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
-void main() => runApp(FlashChat());
+void main() {
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
