826e13442cb420f623e39cd46829c7b3262735b3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 20:27:22 +0530

    import the file cloud_firestore.dart inside the file chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'Chat_Screen';
