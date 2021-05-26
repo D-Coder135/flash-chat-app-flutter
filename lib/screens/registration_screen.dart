5a88576c915ed7552ba232fcaffdf16b4c4121de
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 18:42:41 +0530

    remove the unused import statement from the file registration_screen.dart

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,7 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'Registration_Screen';
