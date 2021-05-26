8297735475e0372b1701e8b8b8713bda2feb3e10
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 19:17:24 +0530

    import the file modal_progress_hud_nsn.dart inside the file login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'Login_Screen';
