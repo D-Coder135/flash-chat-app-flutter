71c40d3bd5998c7cd1266fccc9775aaa7664d882
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 19:11:46 +0530

    set the inAsyncCall property for the ModelProgressHUD() widget

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -21,6 +21,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: ModalProgressHUD(
+        inAsyncCall: isSpinning,
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
