da324e14970f066392c76e9166f40135a189bfd1
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 23:35:25 +0530

    print the message.data inside the body of the for-in loop

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -29,7 +29,9 @@ class _ChatScreenState extends State<ChatScreen> {
 
   void messagesStream() async {
     await for (var snapshot in _firestore.collection('messages').snapshots()) {
-      for (var message in snapshot.docs) {}
+      for (var message in snapshot.docs) {
+        print(message.data);
+      }
     }
   }
 
