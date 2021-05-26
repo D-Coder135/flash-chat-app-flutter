f194749c0cd771700e17f45cdab39097dd10f6c3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 18:59:04 +0530

    remove the print statement from the if() block body from the file chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -18,7 +18,6 @@ class _ChatScreenState extends State<ChatScreen> {
       final user = await _auth.currentUser;
       if (user != null) {
         loggedInUser = user;
-        print(loggedInUser);
       }
     } catch (exception) {
       print(exception);
