410e2473cc4ef35d9a007d035739f7072ef825fd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 20:29:37 +0530

    add a String field name messageText

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -14,6 +14,7 @@ class _ChatScreenState extends State<ChatScreen> {
   final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
   var loggedInUser;
+  String messageText;
 
   void getCurrentUser() async {
     try {
