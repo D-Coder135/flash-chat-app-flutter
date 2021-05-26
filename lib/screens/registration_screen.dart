e37d3cc083a1949e08ef3816d7c84b026ae88e41
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 19:09:27 +0530

    initialize a field of type bool name isSpinning with false value

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -12,6 +12,7 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
+  bool isSpinning = false;
   String email;
   String password;
 
